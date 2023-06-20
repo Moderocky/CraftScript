cb4de939c0995a358f9e5d7826d709157ef58145
Moderocky <admin@moderocky.com>
2023-06-20 10:24:34 +0100

    Add context to script error.

--- src/main/java/mx/kenzie/craftscript/script/ScriptError.java
+++ src/main/java/mx/kenzie/craftscript/script/ScriptError.java
@@ -2,13 +2,24 @@ package mx.kenzie.craftscript.script;
 
 public class ScriptError extends Error {
 
+    private final Context context;
 
     public ScriptError(String message) {
         super(message);
+        this.context = Context.getLocalContext();
     }
 
     public ScriptError(String message, Throwable cause) {
         super(message, cause);
+        this.context = Context.getLocalContext();
+    }
+
+    public Context getContext() {
+        return context;
+    }
+
+    public boolean hasContext() {
+        return context != null;
     }
 
 }
