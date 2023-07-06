08a4dcfffc750c99492a80da34d868419fb7a36d
Moderocky <admin@moderocky.com>
2023-07-06 11:05:29 +0300

    The variable list in 'Kinds.java' was refactored and a new variable type 'UnknownKind' was added. The refactoring was done to directly reference each variable from an imported package or class, instead of referencing from an array, improving code readability and maintainability. 'UnknownKind' was added to handle cases where the variable type cannot be determined, increasing the flexibility of the program.

--- src/main/java/mx/kenzie/craftscript/kind/Kinds.java
+++ src/main/java/mx/kenzie/craftscript/kind/Kinds.java
@@ -1,33 +1,69 @@
 package mx.kenzie.craftscript.kind;
 
+import org.bukkit.block.TileState;
+import org.bukkit.command.CommandSender;
+
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public interface Kinds {
 
-    List<Kind<?>> kinds = new ArrayList<>(List.of(
-        StringKind.STRING,
-        PlayerKind.PLAYER,
-        CommandSenderKind.COMMAND_SENDER,
-        BlockEntityKind.BLOCK_ENTITY,
-        BlockKind.BLOCK,
-        BlockDataKind.BLOCK_DATA,
-        MaterialKind.MATERIAL,
-        LocationKind.LOCATION,
-        VectorKind.VECTOR,
-        StatementKind.STATEMENT,
-        ExecutableKind.EXECUTABLE,
-        EventKind.EVENT,
-        CustomKind.STRUCTURE,
-        MapKind.MAP,
-        ListKind.LIST,
-        CollectionKind.COLLECTION,
-        NullKind.NULL,
-        LibraryKind.LIBRARY,
-        IntegerKind.INTEGER,
-        NumberKind.NUMBER,
-        EnumKind.ENUM,
-        KindKind.KIND
+    StringKind STRING = StringKind.STRING;
+    PlayerKind PLAYER = PlayerKind.PLAYER;
+    CommandSenderKind<CommandSender> COMMAND_SENDER = CommandSenderKind.COMMAND_SENDER;
+    BlockEntityKind<TileState> BLOCK_ENTITY = BlockEntityKind.BLOCK_ENTITY;
+    BlockKind BLOCK = BlockKind.BLOCK;
+    BlockDataKind BLOCK_DATA = BlockDataKind.BLOCK_DATA;
+    MaterialKind MATERIAL = MaterialKind.MATERIAL;
+    LocationKind LOCATION = LocationKind.LOCATION;
+    VectorKind VECTOR = VectorKind.VECTOR;
+    StatementKind STATEMENT = StatementKind.STATEMENT;
+    ExecutableKind EXECUTABLE = ExecutableKind.EXECUTABLE;
+    EventKind EVENT = EventKind.EVENT;
+    CustomKind STRUCTURE = CustomKind.STRUCTURE;
+    MapKind MAP = MapKind.MAP;
+    ListKind LIST = ListKind.LIST;
+    CollectionKind<Collection<?>> COLLECTION = CollectionKind.COLLECTION;
+    NullKind NULL = NullKind.NULL;
+    LibraryKind LIBRARY = LibraryKind.LIBRARY;
+    IntegerKind INTEGER = IntegerKind.INTEGER;
+    NumberKind NUMBER = NumberKind.NUMBER;
+    EnumKind<?> ENUM = EnumKind.ENUM;
+    KindKind KIND = KindKind.KIND;
+
+    /**
+     * Represents an unknown kind of variable.
+     * <p>
+     * The ANY variable is of type UnknownKind and is used to indicate an unknown or unspecified kind of variable.
+     * It can be utilized in cases where the specific kind of variable is not yet or cannot be determined.
+     */
+    UnknownKind ANY = UnknownKind.ANY;
+
+
+    List<Kind<?>> kinds = new ArrayList<>(List.<Kind<?>>of(
+        STRING,
+        PLAYER,
+        COMMAND_SENDER,
+        BLOCK_ENTITY,
+        BLOCK,
+        BLOCK_DATA,
+        MATERIAL,
+        LOCATION,
+        VECTOR,
+        STATEMENT,
+        EXECUTABLE,
+        EVENT,
+        STRUCTURE,
+        MAP,
+        LIST,
+        COLLECTION,
+        NULL,
+        LIBRARY,
+        INTEGER,
+        NUMBER,
+        ENUM,
+        KIND
     ));
 
     static Kind<?>[] values() {
